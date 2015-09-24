ad517ff7e2b487aeefef196a2a0f93b48877a763
Do Minh Hai <do.minh.hai@framgia.com>
2015-09-24 10:55:57 +0700

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ get 'about' => 'static_pages#about'
 # get  'static_pages/contact'
 get 'contact' => 'static_pages#contact'
 
+resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
