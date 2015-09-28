c3003e82a142e613cb4e137193994bc0f9f3b759
Do Minh Hai <do.minh.hai@framgia.com>
2015-09-28 16:18:37 +0700

    add password and reset by email

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'session/new'
 
 #User signup here
@@ -23,6 +27,7 @@ delete 'logout' => 'session#destroy'
 
 resources :users
 resources :account_activations, only: [:edit]
+resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
